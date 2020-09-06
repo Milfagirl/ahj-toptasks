57657b87ee77655224332ca2a4a0dd250cac113f
Svetlana Ryabova <ganyim@inbox.ru>
2020-09-06 23:50:23 +0500

    add pinned

--- src/js/addlist.js
+++ src/js/addlist.js
@@ -10,5 +10,14 @@
     </ul> */}
 
 export default function addList(name) {
-  return `<ul><li class = 'liInTask'><label for="inputList"><div><p>${name}</p><input type="checkbox" id="inputList" name="pin"></div></label></li></ul>`;
+  const div = document.querySelector('.alltasks');
+  const node = div.querySelectorAll('.liInTask');
+  console.log(node);
+  const count = node.length;
+  console.log('count', count);
+  return `<ul><li class = 'liInTask' data-id = ${count}><label for="inputList"><div><p>${name}</p><input type="checkbox" id="inputList" name="pin"></div></label></li></ul>`;
+}
+
+export function addPinnedList(name) {
+  return `<ul><li class = 'liInPinned'><label for="inputList"><div><p>${name}</p><input type="checkbox" id="inputList" name="pin" checked></div></label></li></ul>`;
 }
--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,34 @@
-import addList from './addlist.js';
-
-// document.querySelector('form').addEventListener('keydown', (event) => {
-//     if(event.keyCode === 13) {
-//         event.preventDefault();
-//      }
-// });
+/* eslint-disable prefer-destructuring */
+import addList, { addPinnedList } from './addlist.js';
 
 const elementInput = document.querySelector('#inputform');
-const elementPinned = document.querySelector('.pinned');
+let elementPinned = document.querySelector('.pinned');
 const elementAllTasks = document.querySelector('.alltasks');
+// const inputInAlltasks = elementAllTasks.querySelectorAll('input');
+// Добавляем элементы в tasklist
 elementInput.addEventListener('keydown', (event) => {
-    if(event.keyCode === 13) {
-        event.preventDefault();
-        console.log(elementInput.value);
-        console.log(addList(elementInput.value));
-        elementAllTasks.insertAdjacentHTML('beforeEnd', addList(elementInput.value));
-        elementInput.value = '';
-     }
-//   event.preventDefault();
-// console.log(elementInput.value);
-// console.log(addList(elementInput.value));
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    console.log(elementInput.value);
+    console.log(addList(elementInput.value));
+    elementAllTasks.insertAdjacentHTML('beforeEnd', addList(elementInput.value));
+    elementInput.value = '';
+};
+});
+
+// Добавляем элементы в PinnedList
+document.addEventListener('click', (event) => {
+  if ((event.target.id === 'inputList') && (event.target.closest('[class = alltasks]'))) {
+    if (event.target.checked) {
+      elementPinned = document.querySelector('.pinned');
+      console.log(elementPinned.innerText);
+      if (elementPinned.innerText === 'No pinned tasks') {
+        elementPinned.innerText = '';
+      };
+      const dataid = (event.target.closest('.liInTask').dataset.id);
+      const li = document.querySelector(`li[data-id = '${dataid}']`);
+      const value = li.querySelector('p').innerText;
+      elementPinned.insertAdjacentHTML('beforeEnd', addPinnedList(value));
+    };
+  };   
 });
